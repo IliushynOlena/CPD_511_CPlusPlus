712d04458928881c649de873b25c00a977ca3084
Olena Iliushyn <lenailyshun@gmail.com>
2026-05-23 15:18:42 +0300

    add comment

--- 21_WorkWithFile/21_WorkWithFile.cpp
+++ 21_WorkWithFile/21_WorkWithFile.cpp
@@ -33,33 +33,35 @@ int main()
 	binary_in.close();
 	cout << "Read : " << read.name << " " << read.price << " " << read.isInStoke << endl;
 
-	// 
+	// Текстовий запис до файлу
 	//ofstream fout;
 	//fout.open("text.txt");
-	/*ofstream fout("text.txt");
+	/*ofstream fout("text.txt"); // об*єк, що вміє записувати дані до файлу
 
+	//запис рядків
 	fout << "Work with file ." << endl;
 	fout << "Work with file ." << endl;
 	fout << "Work with file ." << endl;
 	fout << "Work with file ." << endl;
+	//обов*язково файл закриваємо
 	fout.close();*/
 
 
 
 
-	ifstream fin;
-	char buff[255];
-	fin.open("text.txt");
+	ifstream fin;//об*єкт, що зчитує інформацію з файлу
+	char buff[255];//робимо буфер, в який читаємо
+	fin.open("text.txt");//файд відкриваємо
 
-	if (fin.is_open())
+	if (fin.is_open())//перевіряємо чи файл відкритий
 	{
-		fin >> buff;
-		cout << buff;
+		fin >> buff;//зчитуємо до пробілу
+		cout << buff;//показуємо на екран
 
-		while (!fin.eof())
+		while (!fin.eof())//читаємо, поки не кінець файлу
 		{
-			fin.getline(buff, 255);
-			cout << buff << endl;
+			fin.getline(buff, 255);//читаємо по рядках
+			cout << buff << endl;//показуємо на екран
 		}
 		
 	}
@@ -71,7 +73,12 @@ int main()
 	fin.close();
 
 	//ofstream f_out("exaple2.txt", ios_base::out | ios_base::trunc);
-	ofstream f_out("D:\\exaple2.txt", ios_base::app);
+	//ios_base::out;//режим запису
+	//ios_base::in;//режим зчитування
+	//ios_base::app;//режим дозапису
+	//ios_base::trunc;//режим очищення
+	//ios_base::binary;//бінарний режим
+	ofstream f_out("D:\\exaple2.txt", ios_base::app);//відкриваємо в режимі дозапису
 	if (f_out.is_open())
 	{
 		f_out << "Second line" << endl;
