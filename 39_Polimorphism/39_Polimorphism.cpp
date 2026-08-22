0e596c4e779074b2bcf402425c3c50746595962e
Olena Iliushyn <lenailyshun@gmail.com>
2026-08-22 14:31:24 +0300

    Polimorphism

--- 39_Polimorphism/39_Polimorphism.cpp
+++ 39_Polimorphism/39_Polimorphism.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 using namespace std;
-
+//virtual - ovveride - polimorphism
 class Car
 {
 private:
@@ -16,10 +16,10 @@ public:
     Car():mark("no mark"),type_engine("none"), volume(0), year(0), speed(0){}
     Car(string m, string t, float v, int y):mark(m),type_engine(t), 
         volume(v), year(y), speed(0){}
-    void Drive()const {
+    virtual void Drive()const {
         cout << "I am a Car! I can drive with speed : " << speed << "km/h" << endl;
     }
-    void Print()const
+    virtual void Print()const
     {
         cout << "Mark " << mark << endl;
         cout << "Type engine " << type_engine << endl;
@@ -37,11 +37,12 @@ public:
 };
 class PoliceCar : public Car
 {
+
     int soundVolume;
 public:
     PoliceCar(): soundVolume(0), Car(){}
     PoliceCar(string m, string t, float v, int y, int s): soundVolume(s), Car(m,t,v,y){}
-    void Drive()const {
+    void Drive()const override{
         cout << "I am a Police Car! I can drive with speed : " << speed << "km/h" << endl;
     }
     void Print()const
@@ -52,13 +53,49 @@ public:
     void BeepBeep()
     {
         cout << "Beep beep beep with sound volume " << soundVolume << "dB" << endl;
-   }
+    }
 
 };
+class SportCar : public Car
+{
+    int turbo_speed;
+public:
+    SportCar():turbo_speed(0),Car(){}
+    SportCar(string m, string t, float v, int y, int turbo):turbo_speed(turbo),
+        Car(m,t,v,y){}
+    void Drive()const {
+        cout << "I am a Sport Car! I can drive with speed : " << speed << "km/h" << endl;
+    }
+    void Print()const
+    {
+        Car::Print();
+        cout << "Turbo speed " << turbo_speed << endl;
+    }
+    void Nitro()
+    {
+        cout << "Speed before turbo " << speed << " km/h" << endl;
+        speed += turbo_speed;
+        cout << "Speed after turbo " << speed << " km/h" << endl;
+    }
+};
 
+void TestDrive(Car& car)
+{
+    car.setSpeed(70);
+    car.Drive();
+}
 int main()
 {
+    SportCar sportCar("Toyota Supra A80","L6",3.0, 1999, 40);
+    sportCar.setSpeed(250);
+    sportCar.Drive();
+    sportCar.Print();
+    sportCar.Nitro();
     
+
+
+    int marks[10];
+    char marks1[10];
     Car car("Ford focus", "V4", 1.6, 2000);
     PoliceCar police("Dodge Charger", "V6", 3.6, 2016, 500);
     car.setSpeed(120);
@@ -72,10 +109,21 @@ int main()
 
     Car* cars[3] = {
         new Car("Ford focus", "V4", 1.6, 2000),
-        new PoliceCar("Dodge Charger", "V6", 3.6, 2016, 500)
+        new PoliceCar("Dodge Charger", "V6", 3.6, 2016, 500),
+        new SportCar("Toyota Supra A80","L6",3.0, 1999, 40)
     };
+    cout << "=================================" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cars[i]->Drive();
+        cars[i]->Print();
 
-
+        delete cars[i];
+    }
+    cout << "=================================" << endl;
+    TestDrive(car);
+    TestDrive(police);
+    TestDrive(sportCar);
 
 
 }
